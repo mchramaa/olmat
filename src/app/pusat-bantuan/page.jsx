ded7659317cd832f742f113a531e0a6a11fbc1df
mchrama <rama28moch@gmail.com>
2023-11-16 23:21:10 +0700

    back default

--- src/app/pusat-bantuan/page.jsx
+++ src/app/pusat-bantuan/page.jsx
@@ -8,52 +8,17 @@ import {
   MouseParallaxContainer,
 } from "react-parallax-mouse";
 import WaveBackground from "../component/wave";
-import LottiePlayer from "../../../public/assets/lottie/LottiePlayer";
 import pusBanChat from "../../../public/assets/lottie/pusBanChat.json";
+import { Player } from "@lottiefiles/react-lottie-player";
+// import LottiePlayer from "../../../public/assets/lottie/LottiePlayer";
 
 export default function Home() {
   return (
     <main className="bg-white">
-      <MouseParallaxContainer
-        className="parallax"
-        containerStyle={{
-          width: "100%",
-        }}
-        globalFactorX={0.2}
-        globalFactorY={0.2}
-        resetOnLeave>
-        <MouseParallaxChild
-          factorX={0.9}
-          factorY={0.1}
-          style={{
-            background: "url(/assets/paralax/awanPink.png)",
-            backgroundPositionY: "50%",
-            transform: "scale(1.2)",
-            position: "absolute",
-            filter: "blur(4px) brightness(100%)",
-            opacity: "60%",
-            backgroundSize: "auto",
-            backgroundRepeat: "repeat",
-            width: "100%",
-            height: "100%",
-            backfaceVisibility: "hidden",
-          }}
-        />
-        <div className="fixed top-0 z-40">
-          <Nav />
-        </div>
-        <div className="mt-[65px]">
-          <div className=" bg-white grid place-items-center">
-            <LottiePlayer animationData={pusBanChat} />
-            <h1 className=" text-2xl">Pusat Bantuan</h1>
-            <h1 className="font-bold text-xl">
-              Hai, ada yang bisa kami bantu ??
-            </h1>
-          </div>
-        </div>
-        <WaveBackground />
-      </MouseParallaxContainer>
+      <Nav />
       <div className="">
+        <Player src={pusBanChat} loop={true} autoplay={true} className="w-24" />
+        <WaveBackground />
         <Footer />
       </div>
     </main>
